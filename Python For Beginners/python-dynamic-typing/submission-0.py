0c4da419b562f6ab5c259a3a14e1b0e9effad872
Yeswanth Sai Surya Pavan Varma Godavarthi <124238117+pavanvarma058@users.noreply.github.com>
2026-07-20 23:38:00 +0100

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 98.678
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Charlie"
+print(type(variable))
+variable = [1, 22, "string"]
+print(type(variable))
